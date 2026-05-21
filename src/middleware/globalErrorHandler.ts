ca823dc93d582f57b22767112e336a6d48120820
Alamin4D <alaminahmed79000@gmail.com>
2026-05-21 19:02:10 +0600

    fix: improve validation and global error handling

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -3,11 +3,13 @@ import type { NextFunction, Request, Response } from "express";
 const globalErrorHandler = (err: any,req: Request,res: Response,next: NextFunction) => {
   const statusCode = 500;
 
+
   res.status(statusCode).json({
     success: false,
     message: err.message || "Something went wrong",
     errors: err,
   });
 };
+
 
 export default globalErrorHandler;
